3891b0738ce1c745770324d333d61f3a0021e166
Valderli <derlly@ipam.org.br>
2024-11-06 08:20:20 -0300

    ajustes para funcionar com nova estrutura do banco

--- src/components/molecules/FPNDLayer.tsx
+++ src/components/molecules/FPNDLayer.tsx
@@ -1,157 +1,177 @@
 // Importações e dependências
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
-import { Map, Feature } from 'ol';
-import { Geometry } from 'ol/geom';
+import { FC, useCallback, useEffect, useRef, useState } from "react";
+import { Map, Feature } from "ol";
+import { Geometry } from "ol/geom";
 import { MVT } from "ol/format";
-import { RLayerVectorTile} from "rlayers";
+import { RLayerVectorTile } from "rlayers";
 import { useNavigate } from "@tanstack/react-router";
 import { Pixel } from "ol/pixel";
 import { Modal, Button } from "antd";
 
-import {
-    RStyleArray,
-    RStyle,
-    RStroke,
-    RFill,
-    useRStyle,
-} from "rlayers/style";
+import { RStyleArray, RStyle, RStroke, RFill, useRStyle } from "rlayers/style";
 
 const projection = import.meta.env.VITE_DEFAULT_PROJECTION;
 const urlMVT = import.meta.env.VITE_URL_MVT;
 
 type FPNDLayerProps = {
-    mapData: {
-        [x: string]: any
-    },
-    camada: string,
-    esfera: string,
-    estados: Array<string>,
-    pixelClicked: Pixel | null,
-    zIndex?: number
+  mapData: {
+    [x: string]: any;
+  };
+  camada: string;
+  esfera: string;
+  estados: Array<string>;
+  pixelClicked: Pixel | null;
+  zIndex?: number;
 };
 
-export const FPNDLayer: FC<FPNDLayerProps> = ({ mapData, camada, esfera, estados, zIndex, pixelClicked }) => {
-    const navigate = useNavigate();
-    const fpndStyle = useRStyle();
-    const layerRef = useRef<any>(null);
-    const [selectedFeatureCode, setSelectedFeatureCode] = useState<string | null>(null);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [pendingFeatureCode, setPendingFeatureCode] = useState(null);
+export const FPNDLayer: FC<FPNDLayerProps> = ({
+  mapData,
+  camada,
+  esfera,
+  estados,
+  zIndex,
+  pixelClicked,
+}) => {
+  const navigate = useNavigate();
+  const fpndStyle = useRStyle();
+  const layerRef = useRef<any>(null);
+  const [selectedFeatureCode, setSelectedFeatureCode] = useState<string | null>(
+    null
+  );
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pendingFeatureCode, setPendingFeatureCode] = useState(null);
 
-    const data = mapData?.data;
+  const data = mapData?.data;
 
-    // Função para lidar com a confirmação do modal
-    const handleConfirm = () => {
-        setSelectedFeatureCode(pendingFeatureCode);
-        setIsModalVisible(false);
-    };
+  // Função para lidar com a confirmação do modal
+  const handleConfirm = () => {
+    setSelectedFeatureCode(pendingFeatureCode);
+    setIsModalVisible(false);
+  };
 
-    // Função para lidar com o cancelamento do modal
-    const handleCancel = () => {
-        setIsModalVisible(false);
-        setPendingFeatureCode(null)
-    };
+  // Função para lidar com o cancelamento do modal
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setPendingFeatureCode(null);
+  };
 
+  const getColor = useCallback(
+    (codigo: string, baseColor: string, highlightColor: string) => {
+      return selectedFeatureCode === codigo
+        ? highlightColor
+        : camada === ""
+          ? baseColor
+          : data[codigo] && data[codigo][`${camada}Color`]
+            ? data[codigo][`${camada}Color`]
+            : baseColor;
+    },
+    [camada, data, selectedFeatureCode]
+  );
 
-    const getColor = useCallback((codigo: string, baseColor: string, highlightColor: string) => {
-        return selectedFeatureCode === codigo ? highlightColor : camada === '' ? baseColor : data[codigo][`${camada}Color`];
-    }, [camada, data, selectedFeatureCode]);
+  const getStyle = useCallback(
+    (feature: Feature<Geometry>) => {
+      const featureProperties = feature.getProperties();
+      const { codigo, uf, esfera: featureEsfera } = featureProperties;
 
-    const getStyle = useCallback((feature: Feature<Geometry>) => {
-        const featureProperties = feature.getProperties();
-        const { codigo, uf, esfera: featureEsfera } = featureProperties;
+      // Definindo cores como constantes
+      const defaultFillColor = "green";
+      const defaultStrokeColor = "#99999999";
+      const highlightFillColor = "#daa520";
+      const highlightStrokeColor = "#08080899";
+      const filterColor = "#f5a7c140";
 
-        // Definindo cores como constantes
-        const defaultFillColor = 'green';
-        const defaultStrokeColor = '#99999999';
-        const highlightFillColor = '#daa520';
-        const highlightStrokeColor = '#08080899';
-        const filterColor = "#f5a7c140";
+      let colorFill = getColor(codigo, defaultFillColor, highlightFillColor);
+      let colorStroke = getColor(
+        codigo,
+        defaultStrokeColor,
+        highlightStrokeColor
+      );
 
-        let colorFill = getColor(codigo, defaultFillColor, highlightFillColor);
-        let colorStroke = getColor(codigo, defaultStrokeColor, highlightStrokeColor);
+      if (
+        (esfera !== "" && esfera !== featureEsfera) ||
+        (estados.length && !estados.includes(uf))
+      ) {
+        colorFill = filterColor;
+        colorStroke = filterColor;
+      }
 
-        if ((esfera !== '' && esfera !== featureEsfera) || (estados.length && !estados.includes(uf))) {
-            colorFill = filterColor;
-            colorStroke = filterColor;
-        }
-
-        return (
-            <>
-                <RStyle>
-                    <RStroke color={colorStroke} width={1} />
-                    <RFill color={colorFill} />
-                </RStyle>
-            </>
-        );
-    }, [getColor, esfera, estados]);
-    
-    useEffect(() => {
-
-        if(layerRef.current)
-            layerRef.current.ol.changed()
-    }, [camada, esfera, estados, selectedFeatureCode]);
-    
-    useEffect(() => {
-        navigate({ search: (prev) => ({ ...prev, fpnd: selectedFeatureCode || undefined }) });
-    }, [selectedFeatureCode]);
-
-    useEffect(() => {
-        if (pixelClicked) {
-            const map: Map = layerRef.current.context.map;
-            const features = map.getFeaturesAtPixel(pixelClicked);
-            if (features.length > 0) {
-                const feature = features[0]
-                const featureCode = feature.get('codigo');
-                const featureUF = feature.get('uf');
-                const featureEsfera = feature.get('esfera')
-                if ((esfera !== '' && esfera !== featureEsfera) || estados.length && !estados.includes(featureUF)){
-                    setSelectedFeatureCode(null);
-                    return
-                }
-                if ( !pendingFeatureCode || featureCode !== pendingFeatureCode){
-                    setPendingFeatureCode(featureCode);
-                    setIsModalVisible(true);
-                }
-                else{
-                    setSelectedFeatureCode(featureCode)
-                }
-            } else {
-                setSelectedFeatureCode(null);
-                setPendingFeatureCode(null);
-            }
-        }
-    }, [pixelClicked, estados, esfera]);
-
-    return (
+      return (
         <>
-            <Modal
-                title="Confirmar Seleção"
-                visible={isModalVisible}
-                onOk={handleConfirm}
-                onCancel={handleCancel}
-                footer={[
-                    <Button key="ok" type="primary" onClick={handleConfirm}>
-                      Analisar
-                    </Button>
-                  ]}
-            >
-                <p>Deseja analisar a FPND selecionada?</p>
-            </Modal>
-
-            <RStyleArray
-                ref={fpndStyle}
-                render={getStyle}
-            />
-
-            <RLayerVectorTile
-                ref={layerRef}
-                url={urlMVT}
-                projection={projection}
-                format={new MVT()}
-                zIndex={zIndex || undefined}
-                style={fpndStyle}
-            />
+          <RStyle>
+            <RStroke color={colorStroke} width={1} />
+            <RFill color={colorFill} />
+          </RStyle>
         </>
-    );
+      );
+    },
+    [getColor, esfera, estados]
+  );
+
+  useEffect(() => {
+    if (layerRef.current) layerRef.current.ol.changed();
+  }, [camada, esfera, estados, selectedFeatureCode]);
+
+  useEffect(() => {
+    navigate({
+      search: (prev) => ({ ...prev, fpnd: selectedFeatureCode || undefined }),
+    });
+  }, [selectedFeatureCode]);
+
+  useEffect(() => {
+    if (pixelClicked) {
+      const map: Map = layerRef.current.context.map;
+      const features = map.getFeaturesAtPixel(pixelClicked);
+      if (features.length > 0) {
+        const feature = features[0];
+        const featureCode = feature.get("codigo");
+        const featureUF = feature.get("uf");
+        const featureEsfera = feature.get("esfera");
+        if (
+          (esfera !== "" && esfera !== featureEsfera) ||
+          (estados.length && !estados.includes(featureUF))
+        ) {
+          setSelectedFeatureCode(null);
+          return;
+        }
+        if (!pendingFeatureCode || featureCode !== pendingFeatureCode) {
+          setPendingFeatureCode(featureCode);
+          setIsModalVisible(true);
+        } else {
+          setSelectedFeatureCode(featureCode);
+        }
+      } else {
+        setSelectedFeatureCode(null);
+        setPendingFeatureCode(null);
+      }
+    }
+  }, [pixelClicked, estados, esfera]);
+
+  return (
+    <>
+      <Modal
+        title="Confirmar Seleção"
+        open={isModalVisible}
+        onOk={handleConfirm}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="ok" type="primary" onClick={handleConfirm}>
+            Analisar
+          </Button>,
+        ]}
+      >
+        <p>Deseja analisar a FPND selecionada?</p>
+      </Modal>
+
+      <RStyleArray ref={fpndStyle} render={getStyle} />
+
+      <RLayerVectorTile
+        ref={layerRef}
+        url={urlMVT}
+        projection={projection}
+        format={new MVT()}
+        zIndex={zIndex || undefined}
+        style={fpndStyle}
+      />
+    </>
+  );
 };
--- src/components/organisms/Mapa.tsx
+++ src/components/organisms/Mapa.tsx
@@ -1,144 +1,175 @@
-import { Ajuda, Entenda, FPNDLayer, Legenda} from "components/molecules"
-import { Grid } from "antd"
-import { RControl, RLayerTile, RMap } from "rlayers"
-import { useBusiness } from "services/business"
-import { useEffect, useRef, useState } from "react"
+import { Ajuda, Entenda, FPNDLayer, Legenda } from "components/molecules";
+import { Grid } from "antd";
+import { RControl, RLayerTile, RMap } from "rlayers";
+import { useBusiness } from "services/business";
+import { useEffect, useRef, useState } from "react";
 import { useSearch } from "@tanstack/react-router";
 import { Camadas, Esferas } from "services/data";
-import "ol/ol.css"
-import "rlayers/control/layers.css"
-import "assets/styles/mapa.css"
-import { Pixel } from "ol/pixel"
-import { fromLonLat } from 'ol/proj';
-import { DoubleClickZoom, Interaction } from 'ol/interaction'
+import "ol/ol.css";
+import "rlayers/control/layers.css";
+import "assets/styles/mapa.css";
+import { Pixel } from "ol/pixel";
+import { fromLonLat } from "ol/proj";
+import { DoubleClickZoom, Interaction } from "ol/interaction";
 
-const projection = import.meta.env.VITE_DEFAULT_PROJECTION
-const urlTiles = import.meta.env.VITE_URL_GOOGLE_MAP_API_TERRAIN
-
+const projection = import.meta.env.VITE_DEFAULT_PROJECTION;
+const urlTiles = import.meta.env.VITE_URL_GOOGLE_MAP_API_TERRAIN;
 
 const defaultLegend = {
-    title : 'FPND',
-    bins: [
-        {
-            '#008000': 'Florestas'
-        }
-    ],
-}
+  title: "FPND",
+  bins: [
+    {
+      "#008000": "Florestas",
+    },
+  ],
+};
 
 const extents = {
-    "default": [-73.79487377262059, -16.29340772910929, -46.600017512173174, 4.3929018134876925],
-    "AM": [-73.40382673020356, -9.66692010668144, -56.651074667511395, 0.717678199618431],
-    "RO": [-66.80343861008828, -13.67121292568953, -60.08077887914084, -7.975927522993186],
-    "PA": [-58.24996568369845, -9.591853023885147, -46.600017512173174, -0.7190817437694077],
-    "AP": [-52.304820866959, -0.34670336968225535, -49.90229333146293, 4.195350111586374],
-    "MT": [-61.49896585572995, -16.29340772910929, -53.420729312708026, -8.801770729174061],
-    "RR": [-62.00060628713821, -0.3333336122238127, -59.29093707949849, 4.3929018134876925],
-    "AC": [-73.79487377262059, -10.99284924444386, -67.45660048523425, -7.116698090502462]
-}
+  default: [
+    -73.79487377262059, -16.29340772910929, -46.600017512173174,
+    4.3929018134876925,
+  ],
+  AM: [
+    -73.40382673020356, -9.66692010668144, -56.651074667511395,
+    0.717678199618431,
+  ],
+  RO: [
+    -66.80343861008828, -13.67121292568953, -60.08077887914084,
+    -7.975927522993186,
+  ],
+  PA: [
+    -58.24996568369845, -9.591853023885147, -46.600017512173174,
+    -0.7190817437694077,
+  ],
+  AP: [
+    -52.304820866959, -0.34670336968225535, -49.90229333146293,
+    4.195350111586374,
+  ],
+  MT: [
+    -61.49896585572995, -16.29340772910929, -53.420729312708026,
+    -8.801770729174061,
+  ],
+  RR: [
+    -62.00060628713821, -0.3333336122238127, -59.29093707949849,
+    4.3929018134876925,
+  ],
+  AC: [
+    -73.79487377262059, -10.99284924444386, -67.45660048523425,
+    -7.116698090502462,
+  ],
+};
 
-const { useBreakpoint } = Grid
+const { useBreakpoint } = Grid;
 
 export const Mapa = () => {
-    const breakpoints = useBreakpoint()
-    const {
-        entendaIsOpen,
-        mapCenter,
-        mapData,
-        mapZoom,
-        setMapCenter,
-        setMapZoom,
-    } = useBusiness()
-    const { camada, esfera, estados } = useSearch({ from: '/' })
-    const mapRef = useRef<any>(null)
-    const legendData = camada!==undefined && mapData?.layersLegends ? mapData?.layersLegends[Camadas[camada]] : defaultLegend
-    const [ pixelClicked, setPixelClicked ] = useState<Pixel | null> (null)
-    
-    useEffect(() => {
-        if (mapRef.current ) {
-            let extent;
+  const breakpoints = useBreakpoint();
+  const {
+    entendaIsOpen,
+    mapCenter,
+    mapData,
+    mapZoom,
+    setMapCenter,
+    setMapZoom,
+  } = useBusiness();
+  const { camada, esfera, estados } = useSearch({ from: "/" });
+  const mapRef = useRef<any>(null);
+  const legendData =
+    camada !== undefined && mapData?.layersLegends
+      ? mapData?.layersLegends[Camadas[camada]]
+      : defaultLegend;
+  const [pixelClicked, setPixelClicked] = useState<Pixel | null>(null);
 
-            if (estados && estados.length > 0) {               
-                // Calcula a união dos extents para os ufs fornecidos
-                extent = estados.reduce((acc, uf) => {
-                    //@ts-ignore
-                    const ufExtent = extents[uf] || extents['default'];
-                    if (!acc) return ufExtent;
-                    return [
-                        Math.min(acc[0], ufExtent[0]), // xmin
-                        Math.min(acc[1], ufExtent[1]), // ymin
-                        Math.max(acc[2], ufExtent[2]), // xmax
-                        Math.max(acc[3], ufExtent[3])  // ymax
-                    ];
-                }, null);
-            } else {
-                // Usa o extent default
-                extent = extents['default'];
-            }
-            //@ts-ignore
-            extent = [...fromLonLat([extent[0], extent[1]]), ...fromLonLat([extent[2], extent[3]])]
-            mapRef.current.ol.getView().fit(extent, mapRef.current.ol.getSize());
+  useEffect(() => {
+    if (mapRef.current) {
+      let extent;
+
+      if (estados && estados.length > 0) {
+        // Calcula a união dos extents para os ufs fornecidos
+        extent = estados.reduce((acc, uf) => {
+          //@ts-ignore
+          const ufExtent = extents[uf] || extents["default"];
+          if (!acc) return ufExtent;
+          return [
+            Math.min(acc[0], ufExtent[0]), // xmin
+            Math.min(acc[1], ufExtent[1]), // ymin
+            Math.max(acc[2], ufExtent[2]), // xmax
+            Math.max(acc[3], ufExtent[3]), // ymax
+          ];
+        }, null);
+      } else {
+        // Usa o extent default
+        extent = extents["default"];
+      }
+      //@ts-ignore
+      extent = [
+        ...fromLonLat([extent[0], extent[1]]),
+        ...fromLonLat([extent[2], extent[3]]),
+      ];
+      mapRef.current.ol.getView().fit(extent, mapRef.current.ol.getSize());
+    }
+  }, [mapRef, estados]);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.ol
+        .getInteractions()
+        .getArray()
+        .forEach(function (interaction: Interaction) {
+          if (interaction instanceof DoubleClickZoom) {
+            mapRef.current.ol.removeInteraction(interaction);
+          }
+        });
+    }
+  }, [mapRef]);
+
+  return (
+    <RMap
+      width="100%"
+      height="100%"
+      initial={{ center: mapCenter, zoom: mapZoom }}
+      noDefaultControls
+      projection={projection}
+      ref={mapRef}
+      onRenderComplete={(event) => {
+        setMapCenter(event.target.getView().getCenter());
+        setMapZoom(event.target.getView().getZoom());
+      }}
+      onDblClick={(event) => {
+        setPixelClicked(event.pixel);
+      }}
+    >
+      <RLayerTile url={urlTiles} />
+
+      <FPNDLayer
+        zIndex={7}
+        mapData={mapData}
+        camada={camada !== undefined ? Camadas[camada] : ""}
+        esfera={esfera !== undefined ? Esferas[esfera] : ""}
+        estados={estados !== undefined ? estados : []}
+        pixelClicked={pixelClicked}
+      />
+
+      <RControl.RCustom
+        className={
+          breakpoints.xs
+            ? "legendaMobile"
+            : entendaIsOpen
+              ? "legenda"
+              : "legendaCanto"
         }
-    }, [mapRef, estados]);
+      >
+        <Legenda title={legendData.title} bins={legendData.bins} />
+      </RControl.RCustom>
 
-    useEffect(() => {
-        if (mapRef.current ) {
-            mapRef.current.ol.getInteractions().getArray().forEach(function(interaction : Interaction) {
-                if (interaction instanceof DoubleClickZoom) {
-                    mapRef.current.ol.removeInteraction(interaction);
-                }
-            });
-        }
-    }, [mapRef])
+      <RControl.RCustom className="entenda">
+        {!breakpoints.xs && <Entenda />}
+      </RControl.RCustom>
 
-    return (
-        <RMap
-            width="100%"
-            height="100%"
-            initial={{ center: mapCenter, zoom: mapZoom }}
-            noDefaultControls
-            projection={projection}
-            ref={mapRef}
-            onRenderComplete={(event) => {
-                setMapCenter(event.target.getView().getCenter())
-                setMapZoom(event.target.getView().getZoom())
-            }}
-            onDblClick={(event) => {
-                setPixelClicked(event.pixel)
-            }}
-        >
-            <RLayerTile url={urlTiles} />
+      <RControl.RCustom className={breakpoints.xs ? "" : "ajuda"}>
+        {!breakpoints.xs && <Ajuda />}
+      </RControl.RCustom>
 
-            <FPNDLayer 
-                zIndex={7}
-                mapData={mapData} 
-                camada={camada!==undefined ? Camadas[camada] : ''}
-                esfera={esfera!==undefined ? Esferas[esfera] : ''}
-                estados={estados!==undefined ? estados : []}
-                pixelClicked={pixelClicked}
-            />
-            
-            <RControl.RCustom className={breakpoints.xs ? "legendaMobile" : entendaIsOpen ? "legenda" : "legendaCanto"}>
-                <Legenda 
-                    title={legendData.title}
-                    bins = {legendData.bins}
-                />                     
-            </RControl.RCustom>
-
-            <RControl.RCustom className="entenda">
-                { !breakpoints.xs && (
-                    <Entenda />
-                )}
-            </RControl.RCustom>
-
-            <RControl.RCustom className={breakpoints.xs ? "" : 'ajuda'}>
-                { !breakpoints.xs && (
-                    <Ajuda />
-                )}
-            </RControl.RCustom>
-
-            { !breakpoints.xs && (
-                <RControl.RZoom/>
-            )}            
-        </RMap>
-    )
-}
+      {!breakpoints.xs && <RControl.RZoom />}
+    </RMap>
+  );
+};
