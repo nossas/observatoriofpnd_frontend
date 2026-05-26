b9090112a201a2eb1528d7f8a52ba290141ec984
Kenedy Morais <kenedymorais.dev@gmail.com>
2026-05-25 21:49:56 -0500

    feat: inclusao do botao de reportar um problema

--- src/components/molecules/Ajuda.tsx
+++ src/components/molecules/Ajuda.tsx
@@ -1,16 +1,30 @@
 import { Button } from "antd";
 import { QuestionCircleFilled } from '@ant-design/icons';
+import { Bug } from "components/atoms";
 
-const url = import.meta.env.VITE_URL_AJUDA
+const url = import.meta.env.VITE_URL_AJUDA;
+const reportUrl = "https://deolhonasflorestaspublicas.org.br/reportar-um-problema/";
 
 export const Ajuda = () => {
     return (
-        <Button
-            href={url}
-            icon={<QuestionCircleFilled/>}
-            target="_blank"
-            type="default"
-            style={{width:'32px', height:'32px'}}
-        />
-    )
+        <div className="ajuda-buttons">
+            <Button
+                href={url}
+                icon={<QuestionCircleFilled />}
+                target="_blank"
+                rel="noreferrer"
+                type="default"
+                style={{ width: '32px', height: '32px' }}
+            />
+            <Button
+                href={reportUrl}
+                icon={<Bug style={{ width: 16, height: 16 }} />}
+                target="_blank"
+                rel="noreferrer"
+                type="default"
+                style={{ width: '32px', height: '32px' }}
+                title="Reportar um problema"
+            />
+        </div>
+    );
 }
